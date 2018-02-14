f91c48856b6fc409029f0f3ec1d494dc395f7b81
androidgamedev <36428211+androidgamedev@users.noreply.github.com>
2018-02-14 14:08:59 +0100

    Add Dice Anim

---
+++ AndroidGamesDev/app/src/main/java/phong/de/dontlie/DiceAnimationActivity.java
@@ -0,0 +1,70 @@
+package phong.de.dontlie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class DiceAnimationActivity extends AppCompatActivity {
+
+    public static final Random RANDOM = new Random();
+    private Button rollDice;
+    private ImageView imageView1, imageView2;
+
+    public static int randomDiceValue() {
+        return RANDOM.nextInt(6) + 1;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_animation);
+        rollDice = (Button) findViewById(R.id.rollDice);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+        rollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Animation anim1 = AnimationUtils.loadAnimation(DiceAnimationActivity.this, R.anim.shake);
+                final Animation anim2 = AnimationUtils.loadAnimation(DiceAnimationActivity.this, R.anim.shake);
+
+                final Animation.AnimationListener animationListener= new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        int value = randomDiceValue();
+                        int res = getResources().getIdentifier("dice_" +  value, "drawable", "phong.de.dontlie");
+
+                        if (animation == anim1) {
+                            imageView1.setImageResource(res);
+                        } else if (animation == anim2) {
+                            imageView2.setImageResource(res);
+                        }
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                };
+
+                anim1.setAnimationListener(animationListener);
+                anim2.setAnimationListener(animationListener);
+
+                imageView1.startAnimation(anim1);
+                imageView1.startAnimation(anim2);
+            }
+        });
+
+    }
+}
